ca91f6eb3157e05984ef82af934bc9c0a521f700
Flavio Murata <flaviomurata02@gmail.com>
2024-03-25 00:43:07 -0300

    feat: Layed off base structure

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,19 @@
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <div className="Layout">
+      <header className="Layout-header">
+        <h1>Layout</h1>
+      </header>
+      <main className="Layout-main">
+        <p>Content goes here</p>
+      </main>
+      <footer className="Layout-footer">
+        <p>&copy; 2021</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
